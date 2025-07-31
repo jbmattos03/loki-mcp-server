3a22312f288be6e67168fb7acefd7b5168c5b9d6
jbmattos03 <jbmattos2003@gmail.com>
2025-07-31 18:31:37 -0300

    feat: Add servr.py

---
+++ src/server.py
@@ -0,0 +1,9 @@
+from mcp.server.fastmcp import FastMCP
+from logger import logger_config
+from dotenv import load_dotenv
+from os import getenv
+from dataclasses import dataclass
+import requests
+
+load_dotenv()
+
